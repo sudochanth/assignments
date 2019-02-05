0c888a13cf9337335e56a2b51cdf7d13b5323515
v-school <learn@vschool.io>
2019-02-05 16:49:47 -0700

    Update apps.js

--- exercises/mario-pest-control/apps.js
+++ exercises/mario-pest-control/apps.js
@@ -6,7 +6,7 @@ function returnCoin(e) {
     var bobomb = document.getElementById("bobomb").value;
     var cheep = document.getElementById("cheep").value;
     var totalCoins = (goomba * 5) + (bobomb * 7) + (cheep * 11);
-
+// the paranthesis here make this line ^^ extra readable. Good work
     document.getElementsByClassName("totalCoins")[0].innerHTML = `Your total is ${totalCoins} coins!!!`
 
     document.getElementById("goomba").value = "";
@@ -15,3 +15,5 @@ function returnCoin(e) {
 }
 
 document.total.addEventListener("submit", returnCoin);
+
+// You know what you're doing! Good work
