2963f1d69926e5662fa492ccc8e1cb297edd0a0a
Sunny Chanthavong <sudochanth@gmail.com>
2019-02-14 12:23:45 -0700

    feb 14

--- exercises/axios/apps.js
+++ exercises/axios/apps.js
@@ -1,30 +1,39 @@
-// var axios = require("axios");
+// var axios = require("axios"); in place of cdn
 
 axios.get("https://api.vschool.io/sunny/todo").then(function(response) {
     // .map
     response.data.map(function(item) {
         // create elements
+        var bigContainer = document.createElement("div");
+        bigContainer.className = "bigContainer";
         var container = document.createElement("div");
-        container.className = "todoBox";
-        var para = document.createElement("p");
+        container.id = item._id;
         var title = document.createTextNode(`to do: ${item.title}`);
-        var descrip = document.createTextNode(`description: ${item.description}`)
-
+        var descrip = document.createTextNode(`description: ${item.description}`);
+        var price = document.createTextNode(`price: ${item.price}`);
         var input = document.createElement("input");
         input.type = "checkbox";
-        input.id = item._id;
         input.addEventListener("select", function(e) {
-            e.target.style.textDecoration = "line-through";
+            e.target.parentNode.style.textDecoration = "line-through";
         });
         input.className = "input";
+
+        var deleteButton =document.createElement("button");
+        deleteButton.id = "delete";
+        var deleteText = document.createTextNode("delete");
+        deleteButton.appendChild(deleteText);
+        deleteButton.addEventListener("click", deleteObject);
 
         // append
         container.appendChild(input);
         container.appendChild(title);
-        container.appendChild(para);
+        container.appendChild(document.createElement("br"));
         container.appendChild(descrip);
-
-        document.getElementById("todo").appendChild(container);
+        container.appendChild(document.createElement("br"));
+        container.appendChild(price);
+        container.appendChild(document.createElement("br"));
+        bigContainer.appendChild(container);
+        document.getElementById("todo").appendChild(bigContainer);
         
         // create img
         // vvv you can add attributes .src/.className etc
@@ -36,31 +45,72 @@ axios.get("https://api.vschool.io/sunny/todo").then(function(response) {
             container.appendChild(image);
         }
 
+        container.appendChild(document.createElement("br"));
+        container.appendChild(deleteButton);
 
+        // event listener for click
         input.addEventListener("click", handleChecked);
 
         if (item.completed) {
            container.classList.toggle("strikened");
            input.checked = true;
         }
-    })
-
-
+    }); 
 });
 
-
-
-
-
 function handleChecked(e) {
-    console.log(e);
     e.target.parentNode.classList.toggle("strikened");
+    var url = `https://api.vschool.io/sunny/todo/${e.target.parentNode.id}`;
+    if (e.target.parentNode.classList == "strikened") {
+        var newToDo = {
+            "completed": true
+        }
+    } else {
+        var newToDo = {
+            "completed": false
+        }
+    }
+    console.log(newToDo)
+    axios.put(url, newToDo).then(function(response) {
+        console.log(response.data);
+    }).catch(function(error) {
+        console.log(error);
+    });
+    
 }
 
 
+function deleteObject(e) {
+    var url = `https://api.vschool.io/sunny/todo/${e.target.parentNode.id}`;
 
+    axios.delete(url).then(function(response) {
+        console.log(response.data);
+    }).catch(function(error) {
+        console.log(error);
+    });
 
+    alert(`You have deleted a to do object. Refresh page.`)
+}
 
+document.createList.addEventListener("submit", function(e) {
+    e.preventDefault();
+    var url = "https://api.vschool.io/sunny/todo";
+    var newToDo = {
+        "title": document.getElementById("title").value,
+        "description": document.getElementById("description").value,
+        "price": document.getElementById("price").value,
+        "imgUrl": document.getElementById("image").value,
+        "completed": document.getElementById("completed").value
+    };
+
+    axios.post(url, newToDo).then(function(response) {
+        console.log(response.data);
+    }).catch(function(error) {
+        console.log(error);
+    });
+
+    alert(`You have created a to do item. Refresh page.`)
+});
 
 
 
